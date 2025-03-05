bf9cc49d7d312303775c89f7f1bac92f9262abe0
“Austin” <30918019+haywang@users.noreply.github.com>
2025-03-05 09:02:54 +0800

    fix proxy response

--- api/proxy.ts
+++ api/proxy.ts
@@ -1,19 +1,3 @@
-
-
-// module.exports = (request, response) => {
-//     let who = 'anonymous';
-
-//     if (request.body && request.body.who) {
-//       who = request.body.who;
-//     } else if (request.query.who) {
-//       who = request.query.who;
-//     } else if (request.cookies.who) {
-//       who = request.cookies.who;
-//     }
-
-//     response.status(200).send(`Hello ${who}!`);
-//   };
-
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
@@ -33,14 +17,10 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       body: req.method === "GET" ? null : JSON.stringify(req.body),
     });
 
-    const data = await response.json();
-    res.status(response.status).json(data);
+    const textData = await response.text();
+    res.status(response.status).send(textData);
   } catch (error) {
-    res.status(500).json({ error });// "Internal Server Error"
+    console.log(error)
+    res.status(500).json({ error: "Internal Server Error" });
   }
 }
-
-
-// export function GET(request: Request) {
-//   return new Response('Hello from Vercel!');
-// }
